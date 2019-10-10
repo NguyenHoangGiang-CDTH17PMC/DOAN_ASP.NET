d545e44846865250b4147c7c1d9f55019083f1b5
yukihuy99yuki <36331785+yukihuy99yuki@users.noreply.github.com>
2019-10-10 23:17:02 +0700

    Bổ sung một số  thuộc tính cho class thuộc DTO

--- DTO/LoaiSPDTO.cs
+++ DTO/LoaiSPDTO.cs
@@ -12,5 +12,57 @@ namespace DTO
         private string m_tenLoai;
         private bool m_Xoa;
         private string m_ghiChu;
-    }
+
+		public int ID
+		{
+			get
+			{
+				return m_ID;
+			}
+
+			set
+			{
+				m_ID = value;
+			}
+		}
+
+		public string TenLoai
+		{
+			get
+			{
+				return m_tenLoai;
+			}
+
+			set
+			{
+				m_tenLoai = value;
+			}
+		}
+
+		public bool Xoa
+		{
+			get
+			{
+				return m_Xoa;
+			}
+
+			set
+			{
+				m_Xoa = value;
+			}
+		}
+
+		public string GhiChu
+		{
+			get
+			{
+				return m_ghiChu;
+			}
+
+			set
+			{
+				m_ghiChu = value;
+			}
+		}
+	}
 }
